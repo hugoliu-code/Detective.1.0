201613ae69238de645a4535bb3bc51dbf19ffcc9
issuehugo <issuehugounity@gmail.com>
2022-08-18 20:35:43 -0400

    Dummy Enemies in ArtTest

--- Detective.1.0/Assets/Scripts/Player/V1BulletScript.cs
+++ Detective.1.0/Assets/Scripts/Player/V1BulletScript.cs
@@ -7,16 +7,22 @@ public class V1BulletScript : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] LayerMask collidables; //objects, ground, enemies
     private Vector3 previousPosition; //position of the object the frame before
+    private HitManager hm;
+    private bool hasCollided = false;
     private void Start()
     {
+        hm = GameObject.FindGameObjectWithTag("HitManager").GetComponent<HitManager>();
         previousPosition = transform.position;
     }
     private void Update()
     {
+        if (hasCollided) { return; }
         RaycastHit2D collisionCheck = Physics2D.Raycast(previousPosition, transform.position-previousPosition, Vector2.Distance(previousPosition, transform.position), collidables);
         if (collisionCheck)
         {
             HandleCollision(collisionCheck);
+            hasCollided = true;
+
         }
         previousPosition = transform.position;
     }
@@ -24,6 +30,10 @@ public class V1BulletScript : MonoBehaviour
     void HandleCollision(RaycastHit2D collision)
     {
         transform.position = collision.point;
+        AttackData attackData = ScriptableObject.CreateInstance<AttackData>();
+        attackData.damage = 1;
+        attackData.receiver = collision.collider.gameObject;
+        hm.BroadcastHit(attackData);
         Invoke("CustomDestroy",0.01f);
     }
     void CustomDestroy()
