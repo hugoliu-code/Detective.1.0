43c6c2abb7be9114103f68b7283fd54a8d1d05ea
Jakkalantern <jakkalantern@gmail.com>
2022-08-19 17:30:58 -0700

    added a* and audio instance script

---
+++ Detective.1.0/Assets/Scripts/DroneFailsafe.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneFailsafe : MonoBehaviour
+{
+    [SerializeField] private GameObject dest;
+    [SerializeField] private GameObject play;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dest = GameObject.Find("PlayerOffset");
+        play = GameObject.Find("PlayerBody");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (play.transform.localScale[0] == 1f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (play.transform.localScale[0] == -1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        Failsafe();
+    }
+
+    void Failsafe()
+    {
+        //checks the distance betwee the drone and destination
+        Transform destTransform = dest.transform;
+        Vector2 position = destTransform.position;
+        //Debug.Log(Vector2.Distance(position, transform.position));
+        if (Vector2.Distance(position, transform.position) > 15.0)
+        {
+            transform.position = position;
+        }
+    }
+}
