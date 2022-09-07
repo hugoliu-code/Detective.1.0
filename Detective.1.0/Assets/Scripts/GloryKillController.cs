16ca17805256164d5624ef0144c97a7f22ddb450
issuehugo <issuehugounity@gmail.com>
2022-09-06 23:09:31 -0400

    New script and housekeeping

---
+++ Detective.1.0/Assets/Scripts/GloryKillController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloryKillController : MonoBehaviour
+{
+    /*
+     * This should be attached to main player object (the parent)
+     * The script should take care of all events
+     * AFTER a glory kill has been started.
+     * All events before should be taken care of by a player function.
+     * 
+     */
+
+    //Function that moves the player to the correct position to start 
+
+    //Function that moves the robot and player in junction with the animation
+
+}
